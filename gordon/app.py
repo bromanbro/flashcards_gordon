08dffd390f45e0f51f595546f442d8972f9fbcb5
Dorian Yeager <dalorin@gmail.com>
2022-09-22 22:47:24 +0000

    bug correction and adding requirements

--- gordon/app.py
+++ gordon/app.py
@@ -6,24 +6,23 @@ from gordon.card_manager import load_review_cards, promote_card, demote_card
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("cardFile", help="The text file of the deck.")
+    parser.add_argument("card_file", help="The text file of the deck.")
     parser.add_argument("-r", "--reverse", help="Reverse question.", action="store_true")
     parser.add_argument("-s", "--spaced", help="Run Leitner algorithm.", action="store_true")
     args = parser.parse_args()
 
-    if not os.path.isfile("./" + args.cardFile):
+    if not os.path.isfile(args.card_file):
         print("Unable to find that card file.")
         exit(1)
 
-    home = expanduser("~")
-    if not os.path.isdir(home + "/.config"):
-        os.makedirs(home + "/.config")
+    filename = os.path.basename(args.card_file);
 
+    home = expanduser("~")
     if not os.path.isdir(home + "/.config/gordon"):
         os.makedirs(home + "/.config/gordon")
 
-    with dbm.open(home + "/.config/gordon/" + args.cardFile + ".data", "c") as db:
-        reviewCards = load_review_cards(db, args.cardFile, args.spaced, args.reverse)
+    with dbm.open(home + "/.config/gordon/" + filename + ".data", "c") as db:
+        reviewCards = load_review_cards(db, args.card_file, args.spaced, args.reverse)
 
         for card in reviewCards:
             os.system('clear')
